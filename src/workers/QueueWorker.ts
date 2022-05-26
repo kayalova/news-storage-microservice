1fc2476b8c1f1646181e3e6660f0ac1e1751c5a1
Zarema <zarema.klva@gmail.com>
2022-05-26 04:09:47 +0300

    Add queue worker

---
+++ src/workers/QueueWorker.ts
@@ -0,0 +1,41 @@
+import amqp from 'amqplib'
+
+export class QueueWorker {
+    connection: amqp.Connection | undefined
+    channel: amqp.Channel | undefined
+    msgOptions: amqp.Options.Publish | {}
+
+    constructor(channelOptions?: amqp.Options.Publish) {
+        this.connection
+        this.channel
+        this.msgOptions = channelOptions || {}
+    }
+
+    async init() {
+        try {
+            this.connection = await amqp.connect(process.env.RABBITMQ_HOST as string)
+            this.channel = await this.connection.createChannel()
+
+        } catch (error) {
+            console.error(`Rabbitmq error: ${error}`)
+            process.exit(1)
+        }
+    }
+
+    async getChannel() {
+        if (!this.connection || !this.channel) {
+            await this.init()
+        }
+
+        return this.channel
+    }
+
+    async sendMessage(queue: string, msg: any) {
+        if (!this.channel) {
+            await this.getChannel()
+        }
+
+        this.channel!.sendToQueue(queue, msg, this.msgOptions)
+
+    }
+}
